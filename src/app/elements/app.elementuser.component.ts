3cb8807f62e186e76794ff6c03246d9182261d80
Nagaraju <vnagaraju@internal.tavisca.com>
2020-08-19 17:25:53 +0530

    multi column drop down done

--- src/app/elements/app.elementuser.component.ts
+++ src/app/elements/app.elementuser.component.ts
@@ -10,18 +10,28 @@ import { Component, OnInit } from '@angular/core';
       </strong>
     </div>
     <dropdown-element [items]="data" on-selectionChanged="onSelectionChanged($event)"></dropdown-element>
+    <dropdown-element  [enableMultiColumn]="true" [items]="jsonData" on-selectionChanged="onSelectionChanged($event)" ></dropdown-element>
   `
 })
 
 export class ElementUserComponent implements OnInit {
   data: Array<string>;
   receivedValue: string;
+  jsonData:Array<any>;
   constructor() {
     this.data = new Array<string>();
     this.data.push('A');
     this.data.push('B');
     this.data.push('C');
     this.receivedValue = '';
+    this.jsonData = 
+    [
+      { id: 1 , name : "apple" , type: "fruit"},
+      { id: 2 , name : "tomato" , type: "fruit"},
+      { id: 3 , name : "orange" , type: "color"},
+      { id: 4 , name : "red" , type: "color"}
+    ];
+
 
   }
 
